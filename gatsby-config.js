32c95b2b212b497cdfe4703661bb3171719c0a71
MrBorges <lucas.info@gmail.com>
2021-11-30 17:59:57 -0300

    Starting graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `MrBorges Webcorner`,
+    description: `Dad, free thinker, humanist, lover of pizza and beer. Writes code.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const blogPage = () =>{
+    return(
+        <Layout pageTitle ="My blog posts">
+            <p>My cool webpostings will go here.</p>
+        </Layout>
+    )
+}
+
+export default blogPage
