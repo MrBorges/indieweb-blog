f24e9b559dbd30b43952d9588e097e71cf822da9
MrBorges <lucas.info@gmail.com>
2021-11-30 00:17:36 -0300

    Learning about creating components and putting it all together.

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const aboutPage = () =>{
+    return(
+        <Layout pageTitle ="About me">        
+            <p>Hi and welcome to my little web corner. This is an unpretentious dump space for my thoughts, feelings, ideias and ocasional doodles and pictures.</p>
+            <p>I was born in 86 and grew up in a long gone era when we used dial-up cord phones, went to the public library and used actual encyclopedias 
+                to do our homework. I first went online when ICQ was popping all over the place and many kids memorized long UIN numbers to share with friends so we could
+                chat online. Were those better times? Who am I to judge? I just know that I miss the age of the uncentralized web, filled with geocities fansites.
+            </p>
+            <p>I dedicate this little website to those nostalgic days.</p>
+        </Layout>
+        
+    )
+}
+
+export default aboutPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,15 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import * as React from 'react'
+import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+  <Layout pageTitle="Home Page">
+    <p>I'm doing this by following a gatsby tutorial</p>
+    <StaticImage 
+     alt="Silly girl, being silly."
+     src="../images/silly_girl.png" />
   </Layout>
+  
 )
 
 export default IndexPage
