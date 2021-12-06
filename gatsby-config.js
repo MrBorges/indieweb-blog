6bccb04865081cc5ce5aee6556c93b6f791b3161
MrBorges <lucas.info@gmail.com>
2021-12-06 15:02:46 -0300

    Testing PWA functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,14 +28,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,  
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `ExperimentalistBR`,
+        short_name: `ExBR`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#111111`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
